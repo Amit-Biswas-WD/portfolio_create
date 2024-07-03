efc62bb2359ffae511b3d8f9731cc657176ecd84
unknown <amitbiswas9282@gmail.com>
2024-07-04 00:01:51 +0600

    This is Banner section complete

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,15 +1,42 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import image from "../../../assets/image/amit.jpg";
+
 const Banner = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen w-full">
-      <div className="hero-content flex-col lg:flex-row-reverse gap-20">
+    <div className="bg-base-200 mt-10 rounded-xl">
+      <div className="hero-content max-w-[1280px] mx-auto flex-col lg:flex-row-reverse">
         <img
-          src="https://i.ibb.co/DDKmn37/me.jpg"
-          className="max-w-sm rounded-lg shadow-2xl"
+          src={image}
+          className="rounded-lg shadow-2xl lg:my-20 my-10 lg:max-w-[400px] max-w-[220px]"
         />
-        <div>
-          <h1 className="text-5xl font-bold">Hi There, I,m Amit Biswas</h1>
-          <p className="py-6">I am Fond-end Web Developer.</p>
-          <button className="btn btn-primary">Get Started</button>
+        <div className=" lg:pl-28">
+          <h1 className="lg:text-5xl text-3xl font-bold  mb-4 lg:leading-[68px] leading-[40px] lg:mr-72">
+            Hi There, <br />
+            I,m Amit Biswas.
+          </h1>
+          <p className="my-6">I am Font-end Web Developer.</p>
+          <div className="flex gap-4 px-8">
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/amit-biswas-8b22992b4/"
+            >
+              <p className="p-4 rounded-full bg-slate-950 text-[#56c0f1] hover:bg-[#459aee] hover:text-white">
+                <FaLinkedin className="text-2xl " />
+              </p>
+            </a>
+            <a target="_blank" href="https://www.facebook.com/amitbiswas9282">
+              <p className="p-4 rounded-full bg-slate-900 text-[#56c0f1] hover:bg-[#1877F2] hover:text-white">
+                <FaFacebookSquare className="text-2xl" />
+              </p>
+            </a>
+            <a target="_blank" href="https://www.instagram.com/amitbiswas9282/">
+              <p className="p-4 rounded-full bg-slate-900 text-[#56c0f1] hover:bg-[#1877F2] hover:text-white">
+                <FaInstagram className="text-2xl" />
+              </p>
+            </a>
+          </div>
         </div>
       </div>
     </div>
